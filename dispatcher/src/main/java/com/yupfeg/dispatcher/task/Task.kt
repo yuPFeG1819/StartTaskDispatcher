fd9ab5ae3809ff5a291243fb612dda4237594738
yuPFeG <yupfeg1819@foxmail.com>
2022-05-31 18:55:12 +0800

    style : 格式化代码与优化方法名称

--- dispatcher/src/main/java/com/yupfeg/dispatcher/task/Task.kt
+++ dispatcher/src/main/java/com/yupfeg/dispatcher/task/Task.kt
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit
  */
 abstract class Task : ITask, OnTaskStateListener {
 
-    companion object{
+    companion object {
         /**最大等待前置任务时间(s)*/
         internal const val MAX_AWAIT_TIME = 10
     }
@@ -24,43 +24,43 @@ abstract class Task : ITask, OnTaskStateListener {
      * - 默认为类型名称，注意不要与其他任务重名，导致无法进行拓扑排序
      * - 如果使用默认名称，需要避免混淆，从而导致出现重名情况
      * */
-    abstract val tag : String
+    abstract val tag: String
 
     /**
      * 是否需要尽快执行，解决特殊场景的问题：一个Task耗时非常多但是优先级却一般，很有可能开始的时间较晚，
      * 导致最后只是在等它，这种可以早开始。
      * */
-    open val isNeedRunAsSoon : Boolean = false
+    open val isNeedRunAsSoon: Boolean = false
 
     /**
      * 当前任务依赖的前置任务数量（需要等待被依赖的Task执行完毕才能执行自己），默认没有依赖
      * */
     @Volatile
-    private var mDependsLatch : CountDownLatch?= null
+    private var mDependsLatch: CountDownLatch? = null
 
     /**
      * 提供给子类使用的上下文
      * - 注意如果直接创建任务并执行run函数时，该上下文为null，必须在任务调度器内使用才会赋值
      * */
     @Suppress("unused")
-    protected val context : Context
+    protected val context: Context
         get() = mContext ?: throw NullPointerException("you need add this task to Dispatcher")
 
     /**
      * 通常为applicationContext
      * */
-    private var mContext : Context?= null
+    private var mContext: Context? = null
 
     /**
      * 是否监控任务执行
      * */
-    internal val isMonitorTaskOver : Boolean
+    internal val isMonitorTaskOver: Boolean
         get() = mStatusListener != null
 
     /**
      * 任务执行状态回调
      * */
-    private var mStatusListener : OnTaskStateListener? = null
+    private var mStatusListener: OnTaskStateListener? = null
 
     // <editor-fold desc="抽象任务接口实现">
 
@@ -77,12 +77,13 @@ abstract class Task : ITask, OnTaskStateListener {
      * 任务的依赖关系集合，对外为只读状态
      * - 需要先执行的前置任务集合
      * */
-    internal val taskDependsOn : List<String>
+    internal val taskDependsOn: List<String>
         get() = mTaskDependsOnList
+
     /**
      * 任务的依赖关系集合，缓存所有前置任务的唯一标识
      * */
-    private val mTaskDependsOnList : MutableList<String> = mutableListOf()
+    private val mTaskDependsOnList: MutableList<String> = mutableListOf()
 
     /**
      * Task的优先级，运行在主线程则不需要去改优先级，确保主线程优先抢占CPU
@@ -99,8 +100,8 @@ abstract class Task : ITask, OnTaskStateListener {
         mStatusListener?.onTaskWait(tag)
     }
 
-    override fun onTaskStart(tag: String,waitTime: Long) {
-        mStatusListener?.onTaskStart(tag,waitTime)
+    override fun onTaskStart(tag: String, waitTime: Float) {
+        mStatusListener?.onTaskStart(tag, waitTime)
     }
 
     override fun onTaskFinish(runningInfo: TaskRunningInfo) {
@@ -115,9 +116,9 @@ abstract class Task : ITask, OnTaskStateListener {
      * 等待所有前置依赖任务先执行完成，然后再执行当前任务
      * */
     @Throws(InterruptedException::class)
-    internal fun awaitDependsTask(){
+    internal fun awaitDependsTask() {
         val latch = tryGetCountDownLatch()
-        latch.await(MAX_AWAIT_TIME.toLong(),TimeUnit.SECONDS)
+        latch.await(MAX_AWAIT_TIME.toLong(), TimeUnit.SECONDS)
     }
 
     /**
@@ -125,7 +126,7 @@ abstract class Task : ITask, OnTaskStateListener {
      * - 需要注意多线程并发安全问题
      * */
     @Suppress("unused")
-    internal fun dependOverDone(){
+    internal fun dependOverDone() {
         val latch = tryGetCountDownLatch()
         latch.countDown()
     }
@@ -133,8 +134,8 @@ abstract class Task : ITask, OnTaskStateListener {
     /**
      * 尝试获取当前任务的阻塞同步锁
      * */
-    private fun tryGetCountDownLatch() : CountDownLatch{
-        return mDependsLatch?: synchronized(this) {
+    private fun tryGetCountDownLatch(): CountDownLatch {
+        return mDependsLatch ?: synchronized(this) {
             mDependsLatch ?: run {
                 CountDownLatch(
                     if (mTaskDependsOnList.isNullOrEmpty()) 0 else mTaskDependsOnList.size
@@ -150,14 +151,14 @@ abstract class Task : ITask, OnTaskStateListener {
      * - 在添加该任务时赋值上下文，通常为ApplicationContext
      * @param context
      * */
-    internal fun setContext(context: Context){
+    internal fun setContext(context: Context) {
         this.mContext = context.applicationContext
     }
 
     /**
-     * 校验当前任务是否需要主线程等待其完成
+     * 校验当前任务是否处于需要主线程等待其完成
      * */
-    internal fun isNeedMainWaitOver() : Boolean{
+    internal fun isAsyncTaskNeedMainWaitOver(): Boolean {
         return !isRunOnMainThread && isNeedWaitTaskOver
     }
 
@@ -165,7 +166,7 @@ abstract class Task : ITask, OnTaskStateListener {
      * 添加该任务依赖的前置任务类型
      * @param tag 任务的唯一标识
      */
-    internal fun addDependsOnTaskTag(tag : String){
+    internal fun addDependsOnTaskTag(tag: String) {
         //过滤重复任务，避免依赖成环
         if (mTaskDependsOnList.contains(tag)) return
         mTaskDependsOnList.add(tag)
@@ -176,7 +177,7 @@ abstract class Task : ITask, OnTaskStateListener {
      * - 只允许在调度器构造类中设置
      * @param list 任务标识集合
      * */
-    internal fun addDependsOnList(list : List<String>){
+    internal fun addDependsOnList(list: List<String>) {
         for (tag in list) {
             addDependsOnTaskTag(tag)
         }
@@ -187,7 +188,7 @@ abstract class Task : ITask, OnTaskStateListener {
      * - 只允许由调度器统一配置
      * @param listener
      * */
-    internal fun setTaskStateListener(listener: OnTaskStateListener?){
+    internal fun setTaskStateListener(listener: OnTaskStateListener?) {
         mStatusListener = listener
     }
 
