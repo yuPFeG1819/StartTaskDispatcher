3ace5a5c7af9c269277655729b5b6b58af35b5d3
yuPFeG <yupfeg1819@foxmail.com>
2022-06-02 16:59:20 +0800

    feat : 优化测试延迟任务demo

--- app/src/main/java/com/yupfeg/sampledispatcher/MainActivity.kt
+++ app/src/main/java/com/yupfeg/sampledispatcher/MainActivity.kt
@@ -3,21 +3,27 @@ package com.yupfeg.sampledispatcher
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.yupfeg.dispatcher.DelayTaskDispatcher
 import com.yupfeg.dispatcher.ext.*
 import com.yupfeg.logger.ext.loggd
 import com.yupfeg.sampledispatcher.task.InitBDMapTask
 
 class MainActivity : AppCompatActivity() {
+
+    private val mDispatcher : DelayTaskDispatcher by lazy(LazyThreadSafetyMode.NONE) {
+        createDelayDispatcher()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<View>(R.id.btn_test_delay_task).setOnClickListener {
-            performStartUpDelay()
+            mDispatcher.start()
         }
     }
 
-    private fun performStartUpDelay(){
-        startUpDelay(this){
+    private fun createDelayDispatcher() : DelayTaskDispatcher{
+        return startUpDelay(this){
             addTask(InitBDMapTask())
             addTask("delay1") {
                 loggd("延迟任务1 run")
@@ -39,7 +45,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             setOnMonitorRecordListener{recordInfo ->
-                loggd("延迟任务所有任务执行完毕，分发延迟任务的执行记录信息：${recordInfo}")
+                loggd("延迟任务所有任务执行完毕，分发延迟任务的执行记录信息：\n $recordInfo")
             }
 
             setOnDispatcherStateListener{
@@ -51,7 +57,8 @@ class MainActivity : AppCompatActivity() {
                     loggd("延迟任务调度器执行完毕")
                 }
             }
-        }.start()
+        }
     }
+
 
 }
