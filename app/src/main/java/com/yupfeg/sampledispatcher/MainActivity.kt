604ed671a996503ed964778982fd4682bf6fe2a9
yuPFeG <yupfeg1819@foxmail.com>
2022-06-02 16:36:57 +0800

    feat : 新增首页点击开启延迟任务功能

--- app/src/main/java/com/yupfeg/sampledispatcher/MainActivity.kt
+++ app/src/main/java/com/yupfeg/sampledispatcher/MainActivity.kt
@@ -1,34 +1,54 @@
 package com.yupfeg.sampledispatcher
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.yupfeg.dispatcher.ext.addTask
-import com.yupfeg.dispatcher.ext.buildDelayStartUp
-import com.yupfeg.dispatcher.ext.setOnTaskStateListener
-import com.yupfeg.logger.ext.logd
+import com.yupfeg.dispatcher.ext.*
+import com.yupfeg.logger.ext.loggd
+import com.yupfeg.sampledispatcher.task.InitBDMapTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.btn_test_delay_task).setOnClickListener {
+            performStartUpDelay()
+        }
+    }
 
-        buildDelayStartUp(this){
-            addTask {
-                logd("延迟任务1 run")
+    private fun performStartUpDelay(){
+        startUpDelay(this){
+            addTask(InitBDMapTask())
+            addTask("delay1") {
+                loggd("延迟任务1 run")
             }
-            addTask{
-                logd("延迟任务2 run")
+            addTask("delay2"){
+                loggd("延迟任务2 run")
             }
-            addTask {
-                logd("延迟任务3 run")
+            addTask("delay3") {
+                loggd("延迟任务3 run")
             }
 
             setOnTaskStateListener{
                 onStart = {tag, _ ->
-                    logd("$tag 任务开启")
+                    loggd("$tag 任务开启")
                 }
                 onFinished = {info->
-                    logd("延迟任务结束 : $info")
+                    loggd("延迟任务结束 : $info")
+                }
+            }
+
+            setOnMonitorRecordListener{recordInfo ->
+                loggd("延迟任务所有任务执行完毕，分发延迟任务的执行记录信息：${recordInfo}")
+            }
+
+            setOnDispatcherStateListener{
+                onStartBefore = {
+                    loggd("延迟任务开启执行")
+                }
+
+                onFinish = {
+                    loggd("延迟任务调度器执行完毕")
                 }
             }
         }.start()
