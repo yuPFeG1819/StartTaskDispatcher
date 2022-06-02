45abb05f8dce4e8e2529f7eef22bab1ce2ee263d
yuPFeG <yupfeg1819@foxmail.com>
2022-06-02 15:37:32 +0800

    feat : 新增监控类记录携带所有任务执行信息的实体集合，优化调度器执行实体类的的toString逻辑

--- dispatcher/src/main/java/com/yupfeg/dispatcher/monitor/OnMonitorRecordListener.kt
+++ dispatcher/src/main/java/com/yupfeg/dispatcher/monitor/OnMonitorRecordListener.kt
@@ -1,6 +1,7 @@
 package com.yupfeg.dispatcher.monitor
 
 import androidx.annotation.MainThread
+import com.yupfeg.dispatcher.task.TaskRunningInfo
 import java.lang.StringBuilder
 
 /**
@@ -44,24 +45,25 @@ interface OnMonitorRecordListener {
 class DefaultMonitorRecordListener : OnMonitorRecordListener {
 
     /**
-     * 是否处于调试状态
+     * 是否调试任务排序
      * 用于是否遍历输出所有任务的依赖关系，默认为false不会遍历输出依赖关系，提高性能
      * */
     @JvmField
-    var isDebug: Boolean = false
+    var isDebugTaskSort: Boolean = false
 
     /**
      * 任务排序完成回调，返回整理后的所有任务与其中依赖关系的字符串
+     * - 仅在[isDebugTaskSort] = true 时才会回调
      * */
     @JvmField
     var onTaskSorted: ((String) -> Unit)? = null
 
     override val isPrintSortedList: Boolean
-        get() = isDebug
+        get() = isDebugTaskSort
 
     /**
      * 主线程总计消耗时间记录(ms)
-     * - 包括 排序、head任务、调度异步任务、主线程任务执行、主线程等待 的时间
+     * - 包括 任务排序、head任务、调度异步任务、主线程任务执行、主线程等待 的时间
      * */
     var onMainThreadOverRecord: ((Float) -> Unit)? = null
 
@@ -91,27 +93,31 @@ class DefaultMonitorRecordListener : OnMonitorRecordListener {
 data class ExecuteRecordInfo(
     /**所有主线程任务消耗时间(ms)*/
     val allMainTaskCostTime: Float,
-    /**所有任务消耗时间(ms)*/
+    /**所有任务的消耗时间(ms)*/
     val allTaskCostTime: Float,
     /**主线程等待的异步任务数量*/
     val waitAsyncTaskCount: Int,
+    /**主线程等待时间(ms)*/
+    val mainThreadWaitTime : Float,
     /**拓扑排序任务列表消耗时间(ms)*/
     val sortTaskCostTime: Float,
-    /**所有任务的耗时记录map*/
-    val allTaskCostMap: Map<String, Float>
+    /**所有任务的耗时记录信息集合*/
+    val allTaskRunInfoList: List<TaskRunningInfo>
 ) {
     override fun toString(): String {
         val builder = StringBuilder()
         var totalTime = 0f
-        for (entry in allTaskCostMap) {
+        for (entry in allTaskRunInfoList) {
             if (builder.isNotEmpty()) builder.append("\n")
-            builder.append("task tag : ${entry.key} , cost time : ${entry.value} ms")
-            totalTime += entry.value
+            builder.append(entry.toString())
+            totalTime += (entry.runTime + entry.waitTime)
         }
-        return "topological sort task list cost time : $sortTaskCostTime ms , \n" +
-                "all main task cost time : $allMainTaskCostTime ms , \n" +
+        return "sort task list cost time : ${String.format("%.2f",sortTaskCostTime)} ms , \n" +
+                "all main task cost time : ${String.format("%.2f",allMainTaskCostTime)} ms , \n" +
                 "need main thread wait task count : $waitAsyncTaskCount , \n" +
-                "real all task cost time : $allTaskCostTime ms ,\n$builder\n" +
-                "serial total task cost time : $totalTime ms"
+                "main thread await time : ${String.format("%.2f",mainThreadWaitTime)} ms , \n" +
+                "all task total cost time : ${String.format("%.2f",allTaskCostTime)} ms ,\n" +
+                "$builder \n" +
+                "serial execute all task total cost time : ${String.format("%.2f",totalTime)} ms"
     }
 }
